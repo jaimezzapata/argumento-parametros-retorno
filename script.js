95a89d9d4c9d5489d4164a4032fc5f28b3858af8
jaimezzapata <zapataval2304@gmail.com>
2025-12-01 10:11:57 -0500

    Finalizaciónd de función para calcular valorHorasExtras

--- script.js
+++ script.js
@@ -16,6 +16,9 @@
 function iniciarSistemaNominaEmpleado() {
   const VALOR_HORA = prompt("Ingrese el valor de la hora: ");
   const LIMITE_HORAS = prompt("Ingrese el limite de horas ordinarias");
+  const SALARIO_MINIMO = prompt("Ingrese el SMMLV");
+  const AUXILIO_TRANSPORTE = prompt("Ingrese el valor por auxilio de transporte")
+
   if (isNaN(VALOR_HORA) || isNaN(LIMITE_HORAS)) {
     console.log("Debe ingresar solo valores numéricos");
   } else {
@@ -33,13 +36,16 @@ function iniciarSistemaNominaEmpleado() {
       if (opcion == 2) {
         repetir = false;
       } else {
-        calcularSalarioBrutoEmpleado(cantidadHoras, VALOR_HORA, LIMITE_HORAS);
+        let salarioBrutoEmpleado = calcularSalarioBrutoEmpleado(cantidadHoras, VALOR_HORA, LIMITE_HORAS);
+        let valorHorasExtras = calcularValorHoraExtraEmpleado(cantidadHoras, VALOR_HORA, LIMITE_HORAS)
+        let valorAuxilioTransporte = calcularAuxilioTransporteEmpleado(salarioBrutoEmpleado, SALARIO_MINIMO, AUXILIO_TRANSPORTE)
+        console.log(valorAuxilioTransporte)
       }
     }
   }
 }
 
-function calcularSalarioNetoEmpleado() {}
+function calcularSalarioNetoEmpleado() { }
 
 function calcularSalarioBrutoEmpleado(cantidadHoras, VALOR_HORA, LIMITE_HORAS) {
   let salarioEmpleado = 0;
@@ -48,12 +54,31 @@ function calcularSalarioBrutoEmpleado(cantidadHoras, VALOR_HORA, LIMITE_HORAS) {
   } else {
     salarioEmpleado = VALOR_HORA * cantidadHoras;
   }
-  console.log("El salario es: " + salarioEmpleado);
+  return salarioEmpleado
 }
 
-function calcularValorHoraExtraEmpleado() {}
-function calcularAuxilioTransporteEmpleado() {}
-function calcularDeduccionesEmpleado() {}
-function mostrarColillaPagoEmpleado() {}
+function calcularValorHoraExtraEmpleado(cantidadHoras, VALOR_HORA, LIMITE_HORAS) {
+  let valorHorasExtras = 0
+  let cantidadExtras = 0
+  if(cantidadHoras > LIMITE_HORAS){
+    cantidadExtras = cantidadHoras - LIMITE_HORAS
+    valorHorasExtras = cantidadExtras * 0.25 * VALOR_HORA
+    console.log("EL empleado tranajó estas horas extras: " + cantidadExtras)
+  } else {
+    valorHorasExtras = 0
+  }
+  return valorHorasExtras
+}
+
+function calcularAuxilioTransporteEmpleado(salarioBrutoEmpleado, SALARIO_MINIMO, AUXILIO_TRANSPORTE) {
+  if(salarioBrutoEmpleado > SALARIO_MINIMO * 2){
+      return 0
+  } else {
+      return AUXILIO_TRANSPORTE
+  }
+}
+
+function calcularDeduccionesEmpleado() { }
+function mostrarColillaPagoEmpleado() { }
 
 iniciarSistemaNominaEmpleado();
